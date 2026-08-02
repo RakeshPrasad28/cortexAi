acbbf370d337bb111a94e9296da8abbe30a9a03c
Rakeshjvfpl <rakesh.prasad@jvfpl.co>
2026-08-02 18:06:12 +0530

    all agent completed

--- backend/services/agent/agents/ppt.agent.js
+++ backend/services/agent/agents/ppt.agent.js
@@ -1,3 +1,74 @@
-export const pptAgent = async (params) => {
-    
-}
+import { getModel } from "../config/llmModels.js";
+import { generatePpt } from "../utils/generatePpt.js";
+import { getFromS3 } from "../utils/getFromS3.js";
+import { uploadToS3 } from "../utils/uploadToS3.js";
+
+export const pptAgent = async (state) => {
+  try {
+    const llm = await getModel("ppt");
+    const prompt = `
+        You are a professional presentation designer.
+        
+        Return ONLY valid JSON.
+
+        Format:
+
+        {
+            "title":"",
+            "subtitle":"",
+            "slides":[
+                {
+                    "title":"",
+                    "points":[
+                        "",
+                        "",
+                        "",
+                        ""
+                    ]
+                }
+            ]
+        }
+        
+        Rules:
+
+        - Generate exactly 6 content slides.
+        - Each slide should have 4-6 concise bullet points.
+        - No markdown.
+        - No explanation.
+        - No code block.
+        - Return ONLY JSON.
+
+        Topic:
+
+        ${state.prompt}
+    `;
+    const res = await llm.invoke(prompt);
+    const data = JSON.parse(res.content);
+    const ppt = await generatePpt(data);
+    const buffer = await ppt.write({
+      outputType: "nodebuffer",
+    });
+    const fileName = `ppt-${Date.now()}.pptx`;
+    await uploadToS3(
+      fileName,
+      buffer,
+      "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+    );
+    const downloadUrl = await getFromS3(fileName, 24 * 60 * 60);
+    return {
+      ...state,
+      aiResponse: `# ✅ Presentation Generated
+**${data.title}**
+
+📩 [Download PPT](${downloadUrl})
+
+_Link expires in 24 hours._`,
+    };
+  } catch (error) {
+    console.error("PPT Agent Error:", error);
+    return {
+      ...state,
+      aiResponse: "Failed to Generate PPT",
+    };
+  }
+};
